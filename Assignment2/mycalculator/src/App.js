7241c3d4def082556afb402c0a34b15fcd223e7b
anujadhikari-ai <anujadhikari790@gmail.com>
2025-11-30 21:25:57 +0000

    updated assignment2

---
+++ Assignment2/mycalculator/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import KeyPadComponent from "./Calculator";
+
+function App() {
+  return (
+    <div className="App">
+      <KeyPadComponent />
+    </div>
+  );
+}
+
+export default App;
